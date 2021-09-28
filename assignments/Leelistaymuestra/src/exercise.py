17a4041d42552d0ddb67bfdce4b348f83329b82b
JuannAraiza <a01740671@itesm.mx>
2021-09-28 14:02:16 +0000

    resuelto muestra de num en lista

---
+++ assignments/Leelistaymuestra/src/exercise.py
@@ -0,0 +1,18 @@
+def main():
+    #escribe tu código abajo de esta línea
+    n = -1
+
+    while n <= 0:
+        n=int(input('Número de valores a ingresar: '))
+
+    l = []
+
+    for i in range(n):
+        dato = int(input(f'Dame el dato {i}: '))
+        l.append(dato)
+
+    for i in range(n):
+        print(f'lista[{i}]={l[i]}')
+
+if __name__=='__main__':
+    main()
